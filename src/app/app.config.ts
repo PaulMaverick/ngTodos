d8b8a8d8c3f7f0aff1510215c821b42ab7a493ea
Paul <paulpastor122297@gmail.com>
2025-03-03 09:48:34 +0800

    firebase init

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,24 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)]
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp({
+      projectId: "startup-e84fd",
+      appId: "1:830679690152:web:3517acea4ec8b20d6c8b9d",
+      storageBucket: "startup-e84fd.firebasestorage.app",
+      apiKey: "AIzaSyDhVy55cyJ3wQ9x1efZO6qHG5M8HgY2bq8",
+      authDomain: "startup-e84fd.firebaseapp.com",
+      messagingSenderId: "830679690152",
+      measurementId: "G-L42CMFTRFL" })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())]
 };
