0f6dc870f6f78d506cade06bde7c8f1f813e24dc
Paul <paulpastor122297@gmail.com>
2025-03-03 13:24:43 +0800

    working signup

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
 
 @Component({
   selector: 'app-signup',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
+  fb = inject(FormBuilder);
+  http = inject(HttpClient);
+  authService = inject(AuthService)
+  router = inject(Router);
 
+  form = this.fb.nonNullable.group({
+    email: ['', Validators.required],
+    username: ['', Validators.required],
+    password: ['', Validators.required]
+  })
+
+  errorMessage: string | null = null;
+
+  submitHandler() {
+    const rawForm = this.form.getRawValue()
+    this.authService.register(rawForm.email, rawForm.username, rawForm.password).subscribe({
+      next: () => {
+        this.router.navigateByUrl('/')
+      },
+      error: (err) => {
+        this.errorMessage = err.code
+      }
+    })
+  }
 }
